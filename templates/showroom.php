5555f52ed17c5f73fdde51afea7928953f2c551b
Daniel Kudwien <daniel@netzstrategen.com>
2018-02-08 14:52:51 +0100

    Fixed field names, class names, scope of CSS styles, docs and compatibility.

--- templates/showroom.php
+++ templates/showroom.php
@@ -2,36 +2,31 @@
 
 use Netzstrategen\WooCommerceShowroom\Plugin;
 
+$term_id = $term->term_id;
 $image_attr = Plugin::isLazyLoadActive() ? ' data-no-lazy="1"' : '';
 ?>
 
 <div class="showroom gallerya gallerya--slider">
   <ul class="js-gallerya-slider js-gallerya-lightbox">
-    <?php while (have_rows('field_showroom_window', 'product_cat_' . $term_id)): the_row(); ?>
+    <?php while (have_rows('woocommerce-showroom_rooms', 'product_cat_' . $term_id)): the_row(); ?>
+      <?php $image = get_sub_field('image'); ?>
       <li class="showroom__item">
         <figure class="gallerya__image">
-          <?php $image = get_sub_field('field_showroom_image'); ?>
-          <img src="<?= $image['url'] ?>" alt="<?= $image['alt'] ?>" title="<?= $image['title'] ?>" <?= $image_attr ?> />
-          <?php if (!empty($caption = get_sub_field('field_showroom_description'))): ?>
+          <img src="<?= $image['url'] ?>" alt="<?= $image['alt'] ?>" title="<?= $image['title'] ?>"<?= $image_attr ?> />
+          <?php if ($caption = get_sub_field('description')): ?>
             <figcaption class="showroom-item-description"><?= $caption ?></figcaption>
           <?php endif; ?>
         </figure>
-        <?php
-        $related_products = get_sub_field('field_showroom_related_products');
-        if ($related_products) : ?>
-          <ul class="showroom__related-products">
-            <?php foreach ($related_products as $related_product_id): ?>
-            <?php $product = wc_get_product($related_product_id); ?>
-              <li class="showroom__related-product">
-                <div class="showroom__related-product-image">
-                  <a href="<?= $product->get_permalink() ?>" title="<?= $product->get_title() ?>">
-                    <?= $product->get_image() ?>
-                  </a>
+        <?php if ($products = get_sub_field('products')): ?>
+          <ul class="showroom__products">
+            <?php foreach ($products as $product_id): ?>
+              <?php $product = wc_get_product($product_id); ?>
+              <li class="showroom__product">
+                <div class="showroom__product-image">
+                  <a href="<?= $product->get_permalink() ?>" title="<?= $product->get_title() ?>"><?= $product->get_image() ?></a>
                 </div>
-                <div class="showroom__related-products-details">
-                  <a href="<?= $product->get_permalink() ?>" title="<?= $product->get_title() ?>">
-                    <?= $product->get_title() ?>
-                  </a>
+                <div class="showroom__products-details">
+                  <a href="<?= $product->get_permalink() ?>" title="<?= $product->get_title() ?>"><?= $product->get_title() ?></a>
                   <?= $product->get_price_html() ?>
                 </div>
               </li>
